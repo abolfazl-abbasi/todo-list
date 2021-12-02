47eeaa588272957a0f1130d28c44d810571fa638
abolfazl999 <abolcostom99@gmail.com>
2021-12-02 22:09:27 +0330

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./App.jsx-style/App.module.css";
+// import styles from "./App.jsx-style/App.module.css";
 import TodoApp from "./../todo-app/todoApp";
 
 const App = () => {
